530e8da1620581632e5bbbe8e428d6ab03dd0ddb
Davy Paulino <37334813+ThreeDP@users.noreply.github.com>
2024-11-27 20:22:56 +0000

    feat: refactory

--- src/assets/js/Ranking.js
+++ src/assets/js/Ranking.js
@@ -10,13 +10,40 @@ async function ShowRanking(filters, document) {
         listGroup.innerHTML = ""
         ranking.forEach(e => {
             const listItem = document.createElement('div')
+            i = ranking.indexOf(e) + 1;
+            let position = "<i class='m-0 h3 bi bi-dash-circle-fill text-warning'></i>"
+            if (i < 10)
+                position = `<i class="m-0 h3 bi bi-${i}-circle-fill text-success"></i>`
             listItem.innerHTML = `
             <div class="container mt-3">
                 <div class="row border rounded p-2 shadow-sm">
-                    <div class="col-4 font-weight-bold text-primary">Name: <span class="text-dark">${e.name}</span></div>
-                    <div class="col-2">Total Points: <span class="text-success">${e.total_points}</span></div>
-                    <div class="col-2">Wins: <span class="text-success">${e.total_wins}</span></div>
-                    <div class="col-2">Losses: <span class="text-danger">${e.total_losses}</span></div>
+                    <div class="col-3 text-dark d-flex flex-row justify-content-start align-items-center">
+                        ${position}
+                        <div class="d-flex flex-column ps-2">
+                            <p class="m-0"><small>player:</small></p>
+                            <p class="m-0"><strong class="font-weight-bold">${e.name}</strong></p>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center">
+                        <i class="m-0 bi bi-crosshair"></i>
+                        <p class="m-0"><small>Total Points</small></p> 
+                        <p class="m-0"><strong class="text-success">${e.total_points}</strong></p>
+                    </div>
+                    <div class="col-2 text-center">
+                        <i class="m-0 bi bi-trophy-fill"></i>
+                        <p class="m-0"><small>Wins</small></p>
+                        <p class="m-0"><strong class="text-success">${e.total_wins}</strong></p>
+                    </div>
+                    <div class="col-2 text-center">
+                        <i class="m-0 bi bi-shield-fill-x"></i>
+                        <p class="m-0"><small>Losses</small></p>
+                        <p class="m-0"><strong class="text-danger">${e.total_losses}</strong></p>
+                    </div>
+                    <div class="col-2 text-center">
+                        <i class="m-0 bi bi-input-cursor"></i>
+                        <p class="m-0"><small>Draws</small></p>
+                        <p class="m-0"><strong class="text-danger">${e.total_draws}</strong></p>
+                    </div>
                 </div>
             </div>
             `
