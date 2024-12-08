1ece74cc10c2d2893634b4a3f0affae19b0dda41
Lucas de S. Santos <110189896+LukkazZ12@users.noreply.github.com>
2024-12-08 21:37:48 +0000

    Finished game front end.

--- src/assets/js/Enums.js
+++ src/assets/js/Enums.js
@@ -1,13 +1,13 @@
-const gameTypes = {
+const   gameTypes = {
     0: "Match",
     1: "Tournament",
 };
 
-const PlayerColor = {
-    0: "#58A633",
-    1: "#3045BF",
-    2: "#58A633",
-    3: "#D9B54A",
+const   PlayerColor = {
+    0: [88, 166, 51, 100],
+    1: [48, 69, 191, 100],
+    2: [235, 233, 3, 100],
+    3: [234, 41, 13, 100],
 };
 
-const BallColor = "#D92B3A";
+const   BallColor = [255, 255, 255, 100];
