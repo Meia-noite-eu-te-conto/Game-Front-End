f498208d903a5386a988519f85551c3360177277
Everton Vaz <56730329+EvertonVaz@users.noreply.github.com>
2024-12-29 15:25:26 +0000

    merged

--- src/assets/js/EventHandlers.js
+++ src/assets/js/EventHandlers.js
@@ -25,6 +25,7 @@ async function HandleEvents(event, roomCode) {
         modal.show();
         return
     }
+
     if (event.target.dataset.pageName) {
         event.preventDefault()
         await DOMRender(event.target.dataset.pageName);
